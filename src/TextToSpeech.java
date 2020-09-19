5c144213ac99e09f07742c02d8e7a4e4c3a21d0c
Sparye <luffymax0322@gmail.com>
2020-09-19 13:15:42 +1200

    V1espeak

---
+++ src/TextToSpeech.java
@@ -0,0 +1,37 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class TextToSpeech {
+	public static void toSpeech(String msg)  {
+		File file = new File("./GameData/Setting/TTS");
+		String speed="100";
+		Scanner scan;
+		try {
+			scan = new Scanner(file);
+			speed = scan.nextLine();
+			System.out.println(speed);
+			scan.close();
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+
+			e1.printStackTrace();
+		}
+		
+		try {
+			String refinedMsg = "\"" + msg + "\"";
+		//	String command = "espeak "+refinedMsg+" -s 100";
+		//String command = "espeak "+msg;
+			String command = "espeak "+refinedMsg+" -s "+speed;
+
+		//	System.out.println(command);
+			ProcessBuilder pb = new ProcessBuilder("bash", "-c", command);
+			
+			Process process = pb.start();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}
